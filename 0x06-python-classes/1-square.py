e1670c53d24961feae4d7aedd8e58c813d71280a
TaiwoAmodu <42191769+TaiwoAmodu@users.noreply.github.com>
2023-10-25 10:28:19 +0100

    sddsds

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,31 +4,28 @@
 class Square:
     """Represent a square."""
 
+    class Square:
+    """Represent a square."""
+
     def __init__(self, size=0):
         """Initialize a new Square.
 
         Args:
             size (int): The length of each side of the square.
         """
-        self.size = size
+        self.__size = size
 
-    @property
-    def size(self):
-        """Get/set the size length of the square."""
-        return self.__size
+# Instantiate a Square with size 3
+my_square = Square(3)
 
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("Size length must be an integer")
-        if value < 0:
-            raise ValueError("Size length must be >= 0")
-        self.__size = value
+# Check the object's type
+print(type(my_square))
 
-    def area(self):
-        """Return the area of the square."""
-        return self.__size ** 2
+# Access the private attribute via name mangling
+print(my_square.__dict__)
 
-    def perimeter(self):
-        """Return the perimeter of the square."""
-        return 4 * self.__size
+# Try to access the private attribute (this will raise an error)
+try:
+    print(my_square.__size)
+except AttributeError as e:
+    print(e)
