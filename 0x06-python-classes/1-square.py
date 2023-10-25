b206f86e2b3947f49bb63700fad9e8a211cd0c0c
TaiwoAmodu <42191769+TaiwoAmodu@users.noreply.github.com>
2023-10-25 10:01:47 +0100

    sads

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,31 +4,31 @@
 class Square:
     """Represent a square."""
 
-    def __init__(self, side=0):
+    def __init__(self, size=0):
         """Initialize a new Square.
 
         Args:
-            side (int): The length of each side of the square.
+            size (int): The length of each side of the square.
         """
-        self.side = side
+        self.size = size
 
     @property
-    def side(self):
-        """Get/set the side length of the square."""
-        return self.__side
+    def size(self):
+        """Get/set the size length of the square."""
+        return self.__size
 
-    @side.setter
-    def side(self, value):
+    @size.setter
+    def size(self, value):
         if not isinstance(value, int):
-            raise TypeError("Side length must be an integer")
+            raise TypeError("Size length must be an integer")
         if value < 0:
-            raise ValueError("Side length must be >= 0")
-        self.__side = value
+            raise ValueError("Size length must be >= 0")
+        self.__size = value
 
     def area(self):
         """Return the area of the square."""
-        return self.__side ** 2
+        return self.__size ** 2
 
     def perimeter(self):
         """Return the perimeter of the square."""
-        return 4 * self.__side
+        return 4 * self.__size
