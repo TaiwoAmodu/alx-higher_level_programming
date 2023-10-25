3fc6318b5c511fe0dcd983a0ad46d44b40f05068
TaiwoAmodu <42191769+TaiwoAmodu@users.noreply.github.com>
2023-10-25 12:27:27 +0100

    sadsa

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,31 +1,13 @@
 #!/usr/bin/python3
-"""Defines a Square class."""
+"""Square module."""
+
 
 class Square:
-    """Represent a square."""
+    """Defines a square."""
 
-    class Square:
-    """Represent a square."""
-
-    def __init__(self, size=0):
-        """Initialize a new Square.
-
+    def __init__(self, size):
+        """Constructor.
         Args:
-            size (int): The length of each side of the square.
+            size: length of side of the square.
         """
         self.__size = size
-
-# Instantiate a Square with size 3
-my_square = Square(3)
-
-# Check the object's type
-print(type(my_square))
-
-# Access the private attribute via name mangling
-print(my_square.__dict__)
-
-# Try to access the private attribute (this will raise an error)
-try:
-    print(my_square.__size)
-except AttributeError as e:
-    print(e)
