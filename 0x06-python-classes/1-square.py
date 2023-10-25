89818cf252c64d0bca9ecd17ee493ab076c248f8
TaiwoAmodu <42191769+TaiwoAmodu@users.noreply.github.com>
2023-10-25 10:23:21 +0100

    dsds

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -4,9 +4,31 @@
 class Square:
     """Represent a square."""
 
-      def __init__(self, size):
-        """Constructor.
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
         Args:
-            size: length of side of the square.
+            size (int): The length of each side of the square.
         """
-        self.__size = size
+        self.size = size
+
+    @property
+    def size(self):
+        """Get/set the size length of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("Size length must be an integer")
+        if value < 0:
+            raise ValueError("Size length must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
+
+    def perimeter(self):
+        """Return the perimeter of the square."""
+        return 4 * self.__size
